22ec1ba5ecf2491f0167f298409afe9305365d5e
faizmaqsood668@gmail.com <faizmaqsood668@gmail.com>
2024-03-16 02:22:04 +0500

    project completed

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const result = document.querySelector('.result')
 function tossButton(user){
     const toss = Math.ceil(Math.random()*2); 
 if (toss === 1) {
-    image.src = 'https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-18814.jpg'
+    image.src = 'https://flip-a-coin.com/img/coin/fc/heads_vs_tails_1.png'
     }else{
-        image.src = 'https://i.ebayimg.com/images/g/EQcAAOSw5mZhoOxs/s-l1200.webp'
+        image.src = 'https://flip-a-coin.com/img/coin/fc/heads_vs_tails_2.png'
     }
 
-    if (toss === 1 && user === 'Chand' || toss === 2 && user === 'Masjid') {
+    if (toss === 1 && user === 'heads' || toss === 2 && user === 'tails') {
         result.innerHTML = 'Congratulations! You have won the toss! 🎉';
     }else{
         result.innerHTML = 'Looks like luck was not on your side this time. You have lost the toss.';
